0ccd388106b6bca6d7b79b8ff8e22d96990e44f2
Ayotie Channel <117144390+rachmansyahiot@users.noreply.github.com>
2022-12-08 07:43:13 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+# file konfigurasi wifi dan token telegram
+# Ayotie channel
+
+wifi_config = {
+                'ssid':'',
+                'password':''}
+utelegram_config = {
+                'token': ''}
