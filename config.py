2f312624f6fa3173470c4eb7a7774331d2bca412
Ayotie Channel <117144390+rachmansyahiot@users.noreply.github.com>
2022-12-08 07:41:39 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-# file konfigurasi wifi dan token telegram
-# Ayotie channel
-
-wifi_config = {
-                'ssid':'',
-                'password':''}
-utelegram_config = {
-                'token': ''}
